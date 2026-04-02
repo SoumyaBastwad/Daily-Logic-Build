90a31355e6e4b72b1700b1da450dcbfe6f59f452
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-04-02 22:55:43 +0530

    Add files via upload

---
+++ ReverseString.py
@@ -0,0 +1,13 @@
+# reversing the given string by using for loop
+string=input("Enter a string:")
+rev=''
+for i in string:
+    rev=i+rev
+print(rev)
+
+# reversimg string by using slicing metod and creating function for that
+string=input("Enter a string:")
+def RrversingString(string):
+   print(string[::-1])
+
+RrversingString(string)
