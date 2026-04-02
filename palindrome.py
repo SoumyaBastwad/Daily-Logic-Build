d0ca9a3bba9ed81693941013c468863bb6c184a9
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-04-02 22:12:35 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+# cheaking whether the given number is palindrome or not by using while loop
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10          # Get last digit
+    reverse = reverse * 10 + digit
+    num = num // 10           # Remove last digit
+
+if original == reverse:
+    print("Palindrome number")
+else:
+    print("Not a palindrome")
