f3259ca2d5af53943c8946ec9ddac1b003130d15
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-04-07 19:24:27 +0530

    Add files via upload

---
+++ reversingarray.py
@@ -0,0 +1,17 @@
+
+#write a program to reverse the order of element in array
+# sample input:[1,2,3,4,5]
+arr=[1,2,3,4,5]
+print(arr[::-1])
+#or
+arr=[1,2,3,4,5]
+rev=[]
+for i in arr:
+    rev=[i]+rev
+print(rev)
+#or
+arr=[1,2,3,4,5]
+rev=[]
+for i in range(len(arr)-1,-1,-1):
+      rev.append(arr[i])
+print(rev)
