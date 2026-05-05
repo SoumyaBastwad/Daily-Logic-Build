4a5a7c22e189e5465007d4ad147a6e5c75c63a4a
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-05-05 22:48:44 +0530

    Update area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -1,3 +1,10 @@
+# find the area of traingle
+height=float(input("enter the height of traingle:"))
+base=float(input("enter the base of the traingle:"))
+area=(0.5)*height*base
+print("area of the given traingle is:",area)
+
+
 # findig area of traingle by using given height and base
 
 height=float(input("Enter the height of traingle:"))
