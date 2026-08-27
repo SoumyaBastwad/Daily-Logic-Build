0af0189d5950f459f9e7c8c4ee791a742cd8e98f
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-27 21:59:59 +0530

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -5,3 +5,9 @@ for i in string:
     rev=i+rev
 print(rev)
 
+# reversimg string by using slicing metod and creating function for that
+string=input("Enter a string:")
+def RrversingString(string):
+   print(string[::-1])
+
+RrversingString(string)
