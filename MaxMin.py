f44962ad1f224497d826b7490cfd5a9d73f28ba4
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-31 22:42:52 +0530

    Update MaxMin.py

--- MaxMin.py
+++ MaxMin.py
@@ -1,15 +1,1 @@
-numbers = [10, 25, 5, 30, 15]
-
-maximum = numbers[0]
-minimum = numbers[0]
-
-for i in range(1, len(numbers)):
-    if numbers[i] > maximum:
-        maximum = numbers[i]
-
-    if numbers[i] < minimum:
-        minimum = numbers[i]
-
-print("Maximum number:", maximum)
-print("Minimum number:", minimum)
 
