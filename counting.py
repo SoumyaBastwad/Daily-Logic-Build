254fc233c8427408da37e6f98485546c10c3274d
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-14 19:42:57 +0530

    Update counting.py

--- counting.py
+++ counting.py
@@ -1,1 +1,16 @@
+#counting the total number of vowels and consonents in given string
 
+name=input("enter a string:")
+vow=0
+con=0
+
+vowels=['a','e','i','o','u']
+
+for i in name:
+    if i in vowels:
+       vow+=1
+    else:
+        con+=1
+
+print("no of vowels:",vow)
+print("no of consonents:",con)
